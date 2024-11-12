9020e865703b384954fd74743ab162b67b8fdf61
bazarov-07 <bonurbazarov07@gmail.com>
2024-11-12 20:53:47 +0500

    o'zgardi kontakt.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['"Roboto"'],
+    },
     extend: {},
   },
   plugins: [],
